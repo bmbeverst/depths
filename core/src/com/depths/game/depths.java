a1785aabdf643cbec073fd2c957e6b33b6617238
Brooks <bmbeverst@gmail.com>
2018-03-06 22:02:57 -0500

    Moved to Screens and using a camera.

--- core/src/com/depths/game/depths.java
+++ core/src/com/depths/game/depths.java
@@ -2,75 +2,29 @@ package com.depths.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.depths.game.actors.Player;
-import com.depths.game.world.World;
-import com.depths.screens.GameScreen;
 
-// Was ApplicationAdapter originally
-public class depths extends Game{
-	private SpriteBatch batch;
-    private BitmapFont font;
+public class depths extends Game {
 
-    private float elapsedTime = 0;
-	private float deltaTime = 0;
-    
-	private Player player;
-	private World world;
+	SpriteBatch batch;
+	BitmapFont font;
 
-	private Texture img;
-	@Override
-	public void create () {
-
-        Gdx.app.log("depths", "created");
+	public void create() {
 		batch = new SpriteBatch();
+		// Use LibGDX's default Arial font.
 		font = new BitmapFont(Gdx.files.internal("arial-15.fnt"),false);
-        font.setColor(Color.RED);
-        world = new World();
-        player = new Player(world);
-		img = new Texture("badlogic.jpg");
-		setScreen(new GameScreen());
-		
+		this.setScreen(new MainMenuScreen(this));
 	}
 
-	
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        
-        player.update();
-        deltaTime = Gdx.graphics.getDeltaTime();
-
-        world.getLights().render(batch, deltaTime);
-		//batch.begin(); Done in lights
-		batch.draw(img, 0, 0, 1000, 1000);
-        font.draw(batch, "Hello World", 300, 200);
-        world.render(batch);
-        player.draw(batch, elapsedTime);
-		batch.end();
-        elapsedTime += deltaTime;
+	public void render() {
+		super.render(); // important!
 	}
 
-
-
-	@Override
-	public void resize(final int width, final int height) {
-		world.getLights().resize(width, height);
-		super.resize(width, height);
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
 	}
-	
-    @Override
-    public void dispose() {
-        batch.dispose();
-        font.dispose();
-        player.dispose();
-        
-    }
+
 
 }
